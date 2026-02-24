6a8323f42dbf0f3c823fc8c440493ca8e20681ca
Alex Guu <alexwin2099@gmail.com>
2026-02-23 22:33:55 -0500

    small reordering

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
-        <HeroTwo />
         <WhatYourMoneyCanDo />
+        <HeroTwo />
         <Ratings />
         <FAQ />
         <Leaderboard />
